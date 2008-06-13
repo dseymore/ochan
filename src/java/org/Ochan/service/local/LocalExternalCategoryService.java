21ec5906e27234d8de58fae60b4d253bd72b90b5
d.seymore <d.seymore@dde32694-eb45-0410-9153-9728f8fcfaae>
2008-06-13 23:40:45 +0000

    starting the road of having hosts talk to eachother and share lists of categories...
    this is PRE remoting..

---
+++ src/java/org/Ochan/service/local/LocalExternalCategoryService.java
@@ -0,0 +1,69 @@
+package org.Ochan.service.local;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.Ochan.dao.ExternalCategoryDAO;
+import org.Ochan.entity.ExternalCategory;
+import org.Ochan.service.ExternalCategoryService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LocalExternalCategoryService implements ExternalCategoryService {
+
+	private static final Log LOG = LogFactory
+			.getLog(LocalExternalCategoryService.class);
+
+	private ExternalCategoryDAO externalCategoryDAO;
+
+	public ExternalCategoryDAO getExternalCategoryDAO() {
+		return externalCategoryDAO;
+	}
+
+	public void setExternalCategoryDAO(ExternalCategoryDAO externalCategoryDAO) {
+		this.externalCategoryDAO = externalCategoryDAO;
+	}
+
+	@Override
+	public void createCategory(String name, String description, String host) {
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("about to create category with name:" + name
+					+ " and description: " + description);
+		}
+		ExternalCategory cat = new ExternalCategory();
+		cat.setLongDescription(description);
+		cat.setName(name);
+		cat.setHost(host);
+		externalCategoryDAO.saveExternalCategory(cat);
+	}
+
+	@Override
+	public void deleteCategory(Long identifier) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ExternalCategory getCategory(Long identifier) {
+		return externalCategoryDAO.getExternalCategory(identifier);
+	}
+
+	@Override
+	public List<ExternalCategory> retrieveCategories(
+			Map<ExternalCategoryCriteria, String> criteria) {
+		// capture start of call
+		long start = new Date().getTime();
+
+		if (criteria == null || criteria.isEmpty()) {
+			return externalCategoryDAO.getAllExternalCategories();
+		}
+		// capture end of call
+		long end = new Date().getTime();
+		// compute total time
+		// lastSearchTime = end - start;
+
+		return null;
+	}
+
+}
