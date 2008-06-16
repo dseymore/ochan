f0beeea1dc0581c7a929e47130ea616faf7ec5e0
d.seymore <d.seymore@dde32694-eb45-0410-9153-9728f8fcfaae>
2008-06-16 12:19:50 +0000

    missed spring and others in last commit... evil..

---
+++ src/java/org/Ochan/service/remote/model/RemoteCategory.java
@@ -0,0 +1,17 @@
+package org.Ochan.service.remote.model;
+
+public class RemoteCategory {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
