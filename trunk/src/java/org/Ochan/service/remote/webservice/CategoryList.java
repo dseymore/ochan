2cb86beacc8a5fb99599492b0b0d50398d9c75a1
d.seymore <d.seymore@dde32694-eb45-0410-9153-9728f8fcfaae>
2008-06-18 01:54:38 +0000

    ok, not a working client yet, but, server is working with soapui.. client fails.

--- trunk/src/java/org/Ochan/service/remote/webservice/CategoryList.java
+++ trunk/src/java/org/Ochan/service/remote/webservice/CategoryList.java
@@ -5,8 +5,12 @@ import java.util.List;
 import javax.jws.WebService;
 
 import org.Ochan.service.remote.model.RemoteCategory;
+import org.apache.cxf.interceptor.InInterceptors;
+import org.apache.cxf.interceptor.OutInterceptors;
 
 @WebService
+@InInterceptors(interceptors = "org.apache.cxf.interceptor.LoggingInInterceptor")
+@OutInterceptors(interceptors = "org.apache.cxf.interceptor.LoggingOutInterceptor")
 public interface CategoryList {
 
 	public List<RemoteCategory> getCategories();
