10d09c9d3bd0421be1899114624c06c264ea572d
d.seymore <d.seymore@dde32694-eb45-0410-9153-9728f8fcfaae>
2008-11-13 04:02:11 +0000

    Issue #18
    ok, now we are HANDLING the exception, rather than barfing it out in the console.. and forwarding to a friendly velocity page..

---
+++ trunk/src/java/org/ochan/util/ManagedCommonsMultipartResolver.java
@@ -0,0 +1,55 @@
+package org.ochan.util;
+
+import java.util.prefs.Preferences;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+/**
+ * Utility class to wrap the normal XML configurable multipart file dude with a live configurable JMX version
+ * @author David Seymore 
+ * Nov 12, 2008
+ */
+@ManagedResource(objectName = "System:type=config,name=UploadConfiguration", description = "Upload file management", log = true, logFile = "jmx.log")
+public class ManagedCommonsMultipartResolver extends CommonsMultipartResolver {
+	
+	private static final Log LOG = LogFactory.getLog(ManagedCommonsMultipartResolver.class);
+	private static final Preferences PREFERENCES = Preferences.userNodeForPackage(ManagedCommonsMultipartResolver.class);
+	
+	/**
+	 * Sensible default of 1 Mib
+	 */
+	private static final long DEFAULT_MAX_1_MIB = 1000000;
+	
+	/**
+	 * Constructor that auto-starts to the last preferences value
+	 */
+	public ManagedCommonsMultipartResolver(){
+		super();
+		//we have to set the value at startup.. its not used via getter
+		LOG.info("Resetting max file upload size");
+		this.setMaxUploadSize(Long.valueOf(this.getMaxUploadSize()).longValue());
+	}
+	
+	@ManagedAttribute(description="The filesize in bytes for the maximum upload size")
+	public String getMaxUploadSize(){
+		return PREFERENCES.get("maxSize", String.valueOf(DEFAULT_MAX_1_MIB));
+	}
+	
+	@ManagedAttribute(description="The filesize in bytes for the maximum upload size")
+	public void setMaxUploadSize(String value){
+		try{
+			Long val = Long.valueOf(value);
+			//if we get this far, things are all right!
+			PREFERENCES.put("maxSize", value);
+			//call the method of my super!
+			this.setMaxUploadSize(val.longValue());
+		}catch(Exception e){
+			LOG.error("unable to set max file upload size:", e);
+		}
+	}
+	
+}
