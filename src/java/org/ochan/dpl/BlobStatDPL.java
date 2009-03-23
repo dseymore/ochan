2d6aadebf871e921d67097101000069a62269280
d.seymore <d.seymore@dde32694-eb45-0410-9153-9728f8fcfaae>
2009-03-23 10:36:54 +0000

    adding a stat dpl to make computing statistics non invasive.

---
+++ src/java/org/ochan/dpl/BlobStatDPL.java
@@ -0,0 +1,45 @@
+package org.ochan.dpl;
+
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+import com.sleepycat.persist.model.Relationship;
+import com.sleepycat.persist.model.SecondaryKey;
+
+
+@Entity
+public class BlobStatDPL {
+
+	@PrimaryKey(sequence="BLOB_STAT")
+	private Long identifier;
+	
+	@SecondaryKey(relate=Relationship.ONE_TO_ONE)
+	private Long blobIdentifier;
+	private int size;
+	
+	/**
+	 * @return the blobIdentifier
+	 */
+	public Long getBlobIdentifier() {
+		return blobIdentifier;
+	}
+	/**
+	 * @param blobIdentifier the blobIdentifier to set
+	 */
+	public void setBlobIdentifier(Long blobIdentifier) {
+		this.blobIdentifier = blobIdentifier;
+	}
+	/**
+	 * @return the size
+	 */
+	public int getSize() {
+		return size;
+	}
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	
+}
